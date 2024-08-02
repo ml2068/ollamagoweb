32be952d6a77b3bb48cfae5f33b21741a1d545e2
Michael lai <alphaseek@qq.com>
2024-08-02 23:21:45 +0800

    Update app.js
    add random string at the end of output document

--- static/js/app.js
+++ static/js/app.js
@@ -24,7 +24,7 @@ function send(e){
 
 document.getElementById("btnSave").addEventListener("click", () => {
     let date = new Date();
-    let FileName = `${date.getFullYear()}${date.getMonth()+1}${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`.replace(/\s/g, '');
+    let FileName = `${date.getFullYear()}${date.getMonth()+1}${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`.replace(/\s/g, '')+Math.random().toString(36).substring(2,5);
     const txt = document.getElementById('printout').innerHTML+`-----(`+document.getElementById('llmtag').innerText+`)-----`;
     const blob = new Blob([txt], {type: "text/html"});
     const url = URL.createObjectURL(blob);
