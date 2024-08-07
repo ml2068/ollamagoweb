442412074cc1e0c612ceaa635641e2617e9735d5
Michael lai <alphaseek@qq.com>
2024-08-07 22:19:32 +0800

    Update model_show.go
    update more detail show up

--- examples/model_show.go
+++ examples/model_show.go
@@ -8,6 +8,12 @@ import (
     "context"
 )
 
+type Data struct {
+    ModelInfo map[string]interface{} `json:"model_info"`
+    Details   map[string]interface{} `json:"details"`
+    //other data can diret show up 
+}
+
 func main() {
 	//load client
     client, err := api.ClientFromEnvironment()
@@ -18,8 +24,8 @@ func main() {
     ctx := context.Background()
 
     sq := &api.ShowRequest{
-		Model: "llama3.1:8b",
-	}
+	Model: "llama3.1:8b",
+    }
 
     models_show,err:=client.Show(ctx, sq )
     if err != nil {
@@ -27,10 +33,29 @@ func main() {
     }
 
     b, err := json.MarshalIndent(models_show, "   ", "   ")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("\nmodel_show: \n")
-	fmt.Println(string(b))
-
+    if err != nil {
+	log.Fatal(err)
+    }
+    fmt.Println("\nmodel_show: \n")
+    fmt.Println(string(b))
+    
+    //show license
+    fmt.Println("\nlicense: \n ", models_show.License)
+	
+    //show model info
+    var data Data
+    err = json.Unmarshal(b, &data)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+    fmt.Println("\n Model Info: \n")
+    for key, value := range data.ModelInfo {
+        fmt.Printf("%s: %v\n", key, value)
+    }
+    //show model detail
+    fmt.Println("\nModel Detail:\n")
+    for key, value := range data.Details {
+        fmt.Printf("%s: %v\n", key, value)
+    }
 }
